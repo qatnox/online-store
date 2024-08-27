0d275537b92ddf1c16c61441b6ad0318e445ff49
Pavlo <zimnytsia2021ks15@student.karazin.ua>
2024-08-27 09:14:48 +0300

    change app/admin/

--- app/admin/goods.rb
+++ app/admin/goods.rb
@@ -1,3 +1,32 @@
 ActiveAdmin.register Good do
   permit_params :name, :description, :price, :weight, :category_id
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :price
+    column :weight
+    column :category
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :price
+  filter :weight
+  filter :category
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :weight
+      f.input :category
+    end
+    f.actions
+  end
 end
--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,27 @@
 ActiveAdmin.register Order do
   permit_params :user_id, :total_price, :status
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :total_price
+    column :status
+    column :created_at
+    actions
+  end
+
+  filter :user
+  filter :total_price
+  filter :status
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :total_price
+      f.input :status
+    end
+    f.actions
+  end
 end
